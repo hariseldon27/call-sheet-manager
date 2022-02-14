49333104e4d24e36670c100c11ef454bf4cf3107
alexandra <alexandraschurman@gmail.com>
2022-02-14 14:00:25 -0800

    created event files & home

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import StaffContainer from './StaffContainer'
+
+function Home() {
+  return (
+    <div>
+        Home
+        <StaffContainer /> 
+    </div>
+  )
+}
+
+export default Home
---
+++ src/StaffContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import StaffCard from './StaffCard'
+import StaffEditor from './StaffEditor'
+
+function StaffContainer() {
+  return (
+    <div>StaffContainer
+        <StaffCard /> 
+        <StaffEditor />
+    </div>
+  )
+}
+
+export default StaffContainer
