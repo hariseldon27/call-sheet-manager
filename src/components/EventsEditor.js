9949d6feab47bf9a2221c034c54a02a31fe0a09a
alexandra <alexandraschurman@gmail.com>
2022-02-15 07:30:23 -0800

    having trouble with staffPOST, form architecture created

--- src/components/EventsEditor.js
+++ src/components/EventsEditor.js
@@ -1,9 +1,33 @@
 import React from 'react'
 
 function EventsEditor() {
+
+
+
+
   return (
-    <div>EventsEditor</div>
-  )
+    <div>EventsEditor
+      <form>
+     <input 
+        type="text"
+        name="date"
+         placeholder="Date here.."/>         
+    <input 
+      type="text"
+      name="duration"
+      placeholder="Duration here.."/>
+    <input 
+      type="text"
+      name="location"
+      placeholder="Location here.."/>
+      
+    <input 
+       type="text"
+       name="directions"
+       placeholder="Directions here.."/> 
+    </form>
+    </div>
+  );
 }
 
-export default EventsEditor
+export default EventsEditor;
--- src/components/StaffEditor.js
+++ src/components/StaffEditor.js
@@ -1,8 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function StaffEditor() {
+
+const [ newStaff, setNewStaff ] = useState({
+  name: '',
+  department: '',
+  contact: '',
+  calltime: '',
+  notes: ''
+})
+
+function handleStaffSubmit (e){
+  e.preventDefault();
+  fetch('http://localhost:3000/staff', {
+      method: 'POST',
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newStaff)
+  })
+  .then(r => r.json())
+  .then(data => console.log(data))
+}
+
+
+function handleStaffChange(e) {
+  console.log(e.target)
+
+}
+
+
   return (
-    <div>StaffEditor</div>
+    <div>
+      <form onSubmit={handleStaffSubmit}>
+      <h2>Name:</h2>
+    <input 
+        type="text"
+        name="name"
+        placeholder="Name here"/>
+    <input
+        type="text"
+        name="department"
+        placeholder="Department/role here" />
+    <input 
+        type="text"
+        name="contact"
+        placeholder="Contact here" />
+    <input 
+        type="text"
+        name="calltime"
+        placeholder="Calltime here.." />
+    <input
+        type="text"
+        name="notes"
+        placeholder="Notes here.." />
+   
+    <input type="submit" value="Submit" />
+
+    </form>
+
+    </div>
   )
 }
 
